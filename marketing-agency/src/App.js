6b0422538572f78a8804311d25ed66c7193c6dd6
royhandf <royhandaffa14@gmail.com>
2023-06-23 23:03:10 +0700

    updated

---
+++ marketing-agency/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>This is a React app.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
